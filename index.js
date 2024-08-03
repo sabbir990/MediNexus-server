b75842e4486be944547ea3c1aaf35d1dfc43d69c
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-08-03 12:54:27 +0100

    Added : a new route for getting data for a specific medicine through id

--- index.js
+++ index.js
@@ -60,6 +60,20 @@ async function run() {
       const result = await medicineCollection.find(query).toArray();
       res.send(result)
     })
+
+    app.delete('/medicine/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await medicineCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.get('/medicine/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await medicineCollection.findOne(query);
+      res.send(result)
+    })
     
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
