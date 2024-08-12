615ff94e9fb165c2b81b38ddb5f98af26fe90865
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-08-12 17:35:44 +0100

    Added : json webtoken verification, verifyToken, verifyAdmin, verifySeller, verifyUser middlewares and used them

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 8000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2btb5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.use(express.json());
 app.use(cors());
+
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Forbidden access" });
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "Forbidden Access" });
+    }
+
+    console.log(decoded)
+    req.user = decoded;
+    next();
+  })
+}
 
 
 
@@ -31,10 +49,49 @@ async function run() {
     const categoryCollection = client.db("mediNexus").collection('categories');
     const advertisementCollection = client.db("mediNexus").collection('advertisements')
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req?.user?.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      next();
+    }
+
+    const verifySeller = async (req, res, next) => {
+      const email = req?.user?.email;
+      console.log(email)
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isSeller = user?.role === 'seller';
+      if (!isSeller) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      next();
+    }
+
+    const verifyUser = async (req, res, next) => {
+      const email = req?.user?.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isUser = user?.role === 'user';
+      if (!isUser) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      next();
+    }
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ token });
+    })
+
     app.put('/user', async (req, res) => {
       const user = req.body;
       const query = { email: user?.email }
-      // const isExists = await userCollection.findOne(query)
       const options = { upsert: true }
       const updatedDoc = {
         $set: {
@@ -46,43 +103,44 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/user/:email', async (req, res) => {
+    app.get('/user/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await userCollection.findOne(query);
       res.send(result)
     })
 
-    app.post('/medicines', async (req, res) => {
+    app.post('/medicines', verifyToken, verifySeller, async (req, res) => {
       const medicine = req.body;
       const result = await medicineCollection.insertOne(medicine)
       res.send(result)
     })
 
-    app.get('/medicines/:email', async (req, res) => {
+    app.get('/medicines/:email', verifyToken, verifySeller, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await medicineCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.delete('/medicine/:id', async (req, res) => {
+    app.delete('/medicine/:id', verifyToken, verifySeller, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await medicineCollection.deleteOne(query);
       res.send(result)
     })
 
-    app.get('/medicine/:id', async (req, res) => {
+    app.get('/medicine/:id' , async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await medicineCollection.findOne(query);
       res.send(result)
     })
 
-    app.patch('/medicine', async (req, res) => {
+    app.patch('/medicine/:id', verifyToken, verifySeller, async (req, res) => {
+      const id = req.params.id;
       const medicine = req.body;
-      const filter = { _id: new ObjectId(medicine?._id) }
+      const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
         $set: {
           ...medicine
@@ -111,13 +169,13 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/cart', async (req, res) => {
+    app.post('/cart', verifyToken, verifyUser, async (req, res) => {
       const medicine = req?.body;
       const result = await cartCollection.insertOne(medicine);
       res.send(result);
     })
 
-    app.get('/cart/:email', async (req, res) => {
+    app.get('/cart/:email', verifyToken, verifyUser, async (req, res) => {
       const email = req.params.email;
       const query = { buyerEmail: email }
       const result = await cartCollection.find(query).toArray();
@@ -134,7 +192,7 @@ async function run() {
       res.send(groupedData)
     })
 
-    app.delete('/cart/:itemName', async (req, res) => {
+    app.delete('/cart/:itemName', verifyToken, verifyUser, async (req, res) => {
       const itemName = req.params.itemName;
       const query = { itemName: itemName };
       const result = await cartCollection.find(query).toArray();
@@ -146,14 +204,14 @@ async function run() {
       res.send(deletionOperation);
     })
 
-    app.get('/selected-medicine/:itemName', async (req, res) => {
+    app.get('/selected-medicine/:itemName', verifyToken, verifyUser, async (req, res) => {
       const itemName = req.params.itemName;
       const query = { itemName: itemName };
       const result = await cartCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyToken, verifyUser, async (req, res) => {
       try {
         const { discountedPrice } = req.body;
         const amount = discountedPrice * 100;
@@ -181,13 +239,13 @@ async function run() {
       }
     })
 
-    app.post('/billing', async (req, res) => {
+    app.post('/billing', verifyToken, verifyUser, async (req, res) => {
       const billing = req.body;
       const result = await paymentCollection.insertOne(billing);
       res.send(result)
     })
 
-    app.get('/billing-details/:id', async (req, res) => {
+    app.get('/billing-details/:id',verifyToken, verifyUser, async (req, res) => {
       const id = req?.params?.id;
       const query = { _id: new ObjectId(id) };
       const result = await paymentCollection.findOne(query);
@@ -206,7 +264,7 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/category/:id', async (req, res) => {
+    app.delete('/category/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const findItem = await categoryCollection.findOne(query)
@@ -226,18 +284,18 @@ async function run() {
 
     })
 
-    app.post('/category', async (req, res) => {
+    app.post('/category', verifyToken, verifyAdmin, async (req, res) => {
       const category = req.body;
       const result = await categoryCollection.insertOne(category);
       res.send(result)
     })
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result)
     })
 
-    app.patch('/user/:id', async (req, res) => {
+    app.patch('/user/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const role = req?.body;
       const filter = { _id: new ObjectId(id) };
@@ -251,12 +309,12 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/payments', async (req, res) => {
+    app.get('/payments', verifyToken, verifyAdmin, async (req, res) => {
       const result = await paymentCollection.find().toArray();
       res.send(result);
     })
 
-    app.patch('/payment/:id', async (req, res) => {
+    app.patch('/payment/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
@@ -269,14 +327,14 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/payments/:email', async (req, res) => {
+    app.get('/payments/:email', verifyToken, verifySeller, async (req, res) => {
       const email = req.params.email;
       const query = { sellerEmail: email };
       const result = await paymentCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.get('/admin-dashboard', async (req, res) => {
+    app.get('/admin-dashboard', verifyToken, verifyAdmin, async (req, res) => {
       const result = await paymentCollection.find({}, {
         projection: {
           paid_total: 1, status: 1, category: 1
@@ -324,7 +382,7 @@ async function run() {
 
     })
 
-    app.get('/seller-dashboard/:email', async (req, res) => {
+    app.get('/seller-dashboard/:email', verifyToken, verifySeller, async (req, res) => {
       const email = req.params?.email;
       const query = { sellerEmail: email };
       const result = await paymentCollection.find(query, {
@@ -367,29 +425,29 @@ async function run() {
       // Add the header row
       categoryArray.unshift(["Category", "Total Paid"]);
 
-      res.send({total, pendingTotal, paidTotal, categorySpecification: categoryArray });
+      res.send({ total, pendingTotal, paidTotal, categorySpecification: categoryArray });
     })
 
-    app.get('/dashboard-user/:email', async(req, res) => {
+    app.get('/dashboard-user/:email', verifyToken, verifyUser, async (req, res) => {
       const email = req.params.email;
-      const query = {buyerEmail : email};
+      const query = { buyerEmail: email };
       const result = await paymentCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.get('/advertisement-medicines/:email', async(req, res) => {
+    app.get('/advertisement-medicines/:email', verifyToken, verifySeller, async (req, res) => {
       const email = req.params.email;
-      const query = {email : email};
+      const query = { email: email };
       const result = await medicineCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.put('/advertisement-request', async(req, res) => {
+    app.put('/advertisement-request', verifyToken, verifySeller, async (req, res) => {
       const advertisement = req.body;
-      const filter = {itemName : advertisement?.itemName}
-      const options = {upsert : true}
+      const filter = { itemName: advertisement?.itemName }
+      const options = { upsert: true }
       const updatedDoc = {
-        $set : {
+        $set: {
           ...advertisement
         }
       }
@@ -397,51 +455,51 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/all-advertisements", async(req, res) => {
+    app.get("/all-advertisements", verifyToken, verifyAdmin, async (req, res) => {
       const result = await advertisementCollection.find().toArray();
       res.send(result);
     })
 
-    app.patch('/add-advertisement/:id', async (req, res) => {
+    app.patch('/add-advertisement/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-    
+
       try {
         const isAccepted = await advertisementCollection.findOne(filter);
-    
+
         if (!isAccepted) {
           return res.status(404).send({ error: 'Advertisement not found' });
         }
-    
+
         if (isAccepted.status === 'accepted') {
           await advertisementCollection.deleteOne(filter);
           return res.send({ message: 'Advertisement deleted' });
         }
-    
+
         const updateDoc = {
           $set: { status: 'accepted' }
         };
-    
+
         const result = await advertisementCollection.updateOne(filter, updateDoc);
-    
+
         if (result.modifiedCount === 0) {
           return res.status(500).send({ error: 'Failed to update advertisement' });
         }
-    
+
         res.send({ message: 'Advertisement updated successfully' });
-    
+
       } catch (error) {
         console.error(error);
         res.status(500).send({ error: 'An error occurred while processing your request' });
       }
     });
 
-    app.get('/banner-items', async(req, res) => {
-      const query = {status : 'accepted'};
+    app.get('/banner-items', async (req, res) => {
+      const query = { status: 'accepted' };
       const result = await advertisementCollection.find(query).toArray();
       res.send(result);
     })
-    
+
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
